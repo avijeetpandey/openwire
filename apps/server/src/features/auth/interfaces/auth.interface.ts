3f185c8c8cc481f4db945cf49295769be394f9ac
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-11-10 02:39:38 +0530

    Add: joi validator and auth payload

---
+++ apps/server/src/features/auth/interfaces/auth.interface.ts
@@ -0,0 +1,42 @@
+import { Document, ObjectId } from 'mongoose';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: AuthPayload;
+    }
+  }
+}
+
+export interface AuthPayload {
+  userId: string;
+  uId: string;
+  email: string;
+  username: string;
+  avatarColor: string;
+  iat?: number;
+}
+
+export interface IAuthDocument extends Document {
+  _id: string | ObjectId;
+  uId: string;
+  username: string;
+  email: string;
+  password?: string;
+  createdAt: Date;
+  comparePassword(password: string): Promise<boolean>;
+  hashPassword(password: string): Promise<string>;
+}
+
+export interface ISignUpData {
+  _id: ObjectId;
+  uId: string;
+  email: string;
+  username: string;
+  password: string;
+  avatarColor: string;
+}
+
+export interface IAuthJob {
+  value?: string | IAuthDocument;
+}
