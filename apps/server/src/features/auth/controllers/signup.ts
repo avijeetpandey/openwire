447ee7bd5309fdea4bb1207d16ad587b17e034bf
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-11-11 15:25:12 +0530

    Add: Signup controller

---
+++ apps/server/src/features/auth/controllers/signup.ts
@@ -0,0 +1,53 @@
+import { ObjectId } from 'mongodb';
+import { Request, Response } from 'express';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { signupSchema } from '@auth/schemes/signup';
+import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface';
+import { authService } from '@service/db/auth.service';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helpers';
+import { UploadApiResponse } from 'cloudinary';
+import { uploads } from '@global/helpers/cloudinary-upload';
+import HTTP_CODES from 'http-status-codes';
+
+export class SignUp {
+  @joiValidation(signupSchema)
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { username, email, password, avatarColor, avatarImage } = req.body;
+
+    const checkIfUserExists: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
+
+    if (checkIfUserExists) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Helpers.generateRandomIntegers(12)}`;
+    const authData: IAuthDocument = SignUp.prototype.signupData({ _id: authObjectId, uId, username, email, password, avatarColor });
+
+    const result: UploadApiResponse = (await uploads(avatarImage, `${userObjectId}`, true, true)) as UploadApiResponse;
+
+    if (!result?.public_id) {
+      throw new BadRequestError('File upload error occured. Try again');
+    }
+
+    return res.status(HTTP_CODES.CREATED).json({
+      message: 'user created succesfully',
+      data: authData
+    });
+  }
+
+  private signupData(data: ISignUpData): IAuthDocument {
+    const { _id, username, email, avatarColor, uId, password } = data;
+    return {
+      _id,
+      uId,
+      username: Helpers.firstLetterUppercase(username),
+      email: Helpers.lowercase(email),
+      password,
+      avatarColor,
+      createdAt: new Date()
+    } as unknown as IAuthDocument;
+  }
+}
