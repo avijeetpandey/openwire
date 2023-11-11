b75e2735eba1888dcef8b6e2a5bbdf56839b11d6
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-11-11 16:11:14 +0530

    Add: Base queue class

---
+++ apps/server/src/shared/services/queues/base.queue.ts
@@ -0,0 +1,40 @@
+import Queue, { Job } from 'bull';
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { ExpressAdapter } from '@bull-board/express';
+import Logger from 'bunyan';
+import { config } from '@root/config';
+
+let bullAdapters: BullAdapter[] = [];
+
+export let serverAdapter: ExpressAdapter;
+
+// Base class for creating a queue using bull
+export abstract class BaseQueue {
+  queue: Queue.Queue;
+  log: Logger;
+
+  constructor(queueName: string) {
+    this.queue = new Queue(queueName, `${config.REDIS_URL}`);
+    bullAdapters.push(new BullAdapter(this.queue));
+    bullAdapters = [...new Set(bullAdapters)];
+    serverAdapter = new ExpressAdapter();
+    serverAdapter.setBasePath('/queues');
+
+    createBullBoard({ queues: bullAdapters, serverAdapter });
+
+    this.log = config.createLogger(`${queueName}Queue`);
+
+    this.queue.on('completed', (job: Job) => {
+      job.remove();
+    });
+
+    this.queue.on('global:completed', (jobId: string) => {
+      this.log.info(`Job ${jobId} completed`);
+    });
+
+    this.queue.on('global:stalled', (jobId: string) => {
+      this.log.info(`Job ${jobId} stalled`);
+    });
+  }
+}
